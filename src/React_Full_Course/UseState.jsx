78848360df5a3701c43649ef340ed82b9d04145e
Manoj4003 <manojknr16@gmail.com>
2025-09-24 22:01:52 +0530

    update all projects

--- src/React_Full_Course/UseState.jsx
+++ src/React_Full_Course/UseState.jsx
@@ -1,29 +1,8 @@
-import React, { useState } from 'react';
-import './Usestate/UseState.css';
-
-
-const UseState = () => {
-    const [UserName,setUserName]=useState("Manoj");
-    const [UserAge,setUserAge]=useState(24);
-    function changename()
-    {
-      UserName==="Manoj"? setUserName("Kumar"):setUserName("Manoj");
-    }
-    function changeAge()
-
-
-    {
-        UserAge===25?setUserAge(24):setUserAge(25);
-    }
-  return (
-    <div>
-      <h1>User Details</h1>
-      <h1>{UserName}</h1>
-      <h3>{UserAge}</h3>
-      <button onClick={changename}>Update Name</button>
-        <button onClick={changeAge}>Change Age</button>
-    </div>
+import React from "react";
+const UseState=()=>{
+  return(
+    <>
+    </>
   )
-}
-
-export default UseState
+};
+export default UseState;
